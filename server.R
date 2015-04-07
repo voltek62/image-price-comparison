d14df2b4eac617624b68e09576fa8e2762813b40
Vincent Terrasi <vterrasi@oxygem.tv>
2015-04-08 01:20:30 +0200

    improve interfac

--- server.R
+++ server.R
@@ -11,11 +11,24 @@ library(RCurl)
 library(XML)
 library(stringr)
 
+# Load the ggplot2 package which provides
+# the 'mpg' dataset.
+library(ggplot2)
+
 shinyServer(function(input, output) {
-  output$contents <- renderText({
+  
+  output$image <- renderUI({
     
     url_src <- "http://s3.static69.com/m/image-offre/c/e/a/f/ceaf8b49bb662bcb8fbf15858feecdea-500x500.jpg"
-    u <- paste("http://images.google.com/searchbyimage?hl=en&image_url=",url_src,sep="")
+    
+    tags$img(src = url_src, width = "200px")
+    
+  })
+  
+  output$debug <- renderText({
+    
+    url_src <- "http://s3.static69.com/m/image-offre/c/e/a/f/ceaf8b49bb662bcb8fbf15858feecdea-500x500.jpg"
+    u <- paste("http://images.google.com/searchbyimage?hl=",input$country,"&image_url=",url_src,sep="")
     useragent <- "Mozilla/5.0 (iPhone; CPU iPhone OS 6_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10A5376e Safari/8536.25"
     html <- getURL(u,ssl.verifypeer = FALSE, httpheader = c('User-Agent' = useragent))
     doc <- htmlParse(html)
@@ -23,15 +36,48 @@ shinyServer(function(input, output) {
     links <- xpathSApply(doc, "//a/@href")
         
     u <- paste(links[[1]],"&gws_rd=ssl",sep="")
+    u
+    
+  })
+  
+  # Filter data based on selections
+  output$table <- renderDataTable({
+#    data <- mpg
+#     if (input$cyl != "All"){
+#       data <- data[data$cyl == input$cyl,]
+#     }
+#     if (input$trans != "All"){
+#       data <- data[data$trans == input$trans,]
+#     }
+#    data
+    url_src <- "http://s3.static69.com/m/image-offre/c/e/a/f/ceaf8b49bb662bcb8fbf15858feecdea-500x500.jpg"
+    u <- paste("http://images.google.com/searchbyimage?hl=",input$country,"&image_url=",url_src,sep="")
+    useragent <- "Mozilla/5.0 (iPhone; CPU iPhone OS 6_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10A5376e Safari/8536.25"
+    html <- getURL(u,ssl.verifypeer = FALSE, httpheader = c('User-Agent' = useragent))
+    doc <- htmlParse(html)
+    #HTML(doc,file="test.html")
+    links <- xpathSApply(doc, "//a/@href")
+    
+    u <- paste(links[[1]],"&gws_rd=ssl",sep="")
     useragent <- "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36"
     html <- getURL(u,ssl.verifypeer = FALSE, httpheader = c('User-Agent' = useragent))
+    HTML(doc,file="list.html")
     doc <- htmlParse(html)
     # extraction
     # on remplit le dataframe
-    attrs <- xpathApply(doc, "//h3[@class='r']/a/@href")
+    attrs <- xpathApply(doc, "//h3[@class='r']/a/@href")  
+    attrs2 <- xpathApply(doc, "//h3[@class='r']/a")   
+    attrs3 <- xpathApply(doc, "//a/img/@src")
     free(doc)
     links <- sapply(attrs, function(x) x[[1]])
+    titles <- sapply(attrs2, function(x) x[[1]])
     
+    print(titles)
     
-  })
+    df <- data.frame(links)       
+ 
+    df
+    
+  })  
+  
 })
--- ui.R
+++ ui.R
@@ -5,25 +5,56 @@
 # http://shiny.rstudio.com
 #
 
+# 
+# shinyUI(bootstrapPage(
+# 
+#   textInput("url", label = h3("URL Image"), value = ""),
+#   tags$style(type='text/css', "#url { width: 100%; }"),
+#     
+#   tableOutput('contents'),
+#   
+#   dataTableOutput(outputId="table")
+# 
+# ))
+
+
 library(shiny)
 
-shinyUI(pageWithSidebar(
-  headerPanel("Image Price Comparison"),
-  sidebarPanel(
+# Load the ggplot2 package which provides
+# the 'mpg' dataset.
+library(ggplot2)
+
+# Define the overall UI
+shinyUI(
+  fluidPage(
+    titlePanel("Image Price Comparison"),
     
-    textInput("url", label = h3("URL Image"), 
-              value = ""),
-    
-    fileInput('file1', 'Choose CSV File',
-              accept=c('text/csv', 'text/comma-separated-values,text/plain', '.csv')),
-    
-    
-    tags$hr(),
-    
-    checkboxInput('header', 'Header', TRUE),
-    
-  ),
-  mainPanel(
-    tableOutput('contents')
-  )
-))
+    # Create a new Row in the UI for selectInputs
+     fluidRow(
+       column(8, 
+          textInput("url", label = h3("URL Image"), value = "http://s3.static69.com/m/image-offre/c/e/a/f/ceaf8b49bb662bcb8fbf15858feecdea-500x500.jpg"),
+          tags$style(type='text/css', "#url { width: 100%; }")
+       ),
+       column(4, 
+              selectInput("country", 
+                          label = h3("Country"), 
+                          c("com", "fr"))
+       )        
+     ),
+
+#     fluidRow(
+#       column(4,
+#         htmlOutput('image')     
+#       ),
+#       column(8,
+#         dataTableOutput(outputId="table")     
+#       )
+#     ),  
+      
+    fluidRow(
+      htmlOutput('image'),
+      dataTableOutput(outputId="table"),
+      tableOutput('debug')
+    )    
+  )  
+)
